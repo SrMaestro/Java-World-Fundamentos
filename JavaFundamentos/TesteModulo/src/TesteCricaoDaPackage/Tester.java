74fc8f19b1f7de8dc367626b6417b707ad6d62e4
Luis Felipe R . L <luisramires29032002@gmail.com>
2023-12-22 01:31:33 -0300

    conclusao dos exercicios 1 ao 4

--- JavaFundamentos/TesteModulo/src/TesteCricaoDaPackage/Tester.java
+++ JavaFundamentos/TesteModulo/src/TesteCricaoDaPackage/Tester.java
@@ -1,2 +1,7 @@
-package TesteCricaoDaPackage;public class Tester {
+package TesteCricaoDaPackage;
+
+public class Tester {
+    public static void main(String[] args) {
+        System.out.println("ola mundo");
+    }
 }
