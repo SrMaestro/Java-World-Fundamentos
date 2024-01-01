e599a94c7911eecaac387e20f406f514eb5864a9
Luis Felipe R . L <luisramires29032002@gmail.com>
2024-01-01 09:38:44 -0300

    criaçao da funcionalidade depositar e alguns ajuste com a alteraçao do saldo

--- JavaFundamentos/src/Aula02/ContaBancaria.java
+++ JavaFundamentos/src/Aula02/ContaBancaria.java
@@ -1,2 +1,16 @@
-package Aula02;public class ContaBancaria {
+package Aula02;
+
+import java.time.LocalDateTime;
+
+public class ContaBancaria {
+
+    private String nome;
+    private int cpf;
+    private int identificadorConta;
+    private String banco;
+    private String endereco;
+    private double saldo;
+    private LocalDateTime horarioAtual;
+
+
 }
